f5ad7ddae7cdcd4ef4f52e5bb6d90b2de219b7c4
Aditya Singh <adityasinghparmar60@gmail.com>
2026-01-13 23:45:43 +0530

    Changed analysis pattern

--- data_analysis.py
+++ data_analysis.py
@@ -6,3 +6,26 @@ with open('cleaned_commit.json','r') as f:
 
 df=pd.DataFrame(data)
 print(df.head())
+
+df['date']=pd.to_datetime(df['date'])
+daily_commits = (
+    df
+    .set_index('date')
+    .resample('D')
+    .size()
+    .reset_index(name='commit_count')
+)
+
+author_commit_count = (
+    df
+    .groupby("author")
+    .size()
+    .reset_index(name="total_commits")
+)
+
+active_days=daily_commits[daily_commits['commit_count']>0]
+consistency = len(active_days) / len(daily_commits)
+print(f"Consistency: {consistency:.2%}")
+
+df.to_csv("commits_clean.csv", index=False)
+author_commit_count.to_csv("author_commit_count.csv", index=False)
