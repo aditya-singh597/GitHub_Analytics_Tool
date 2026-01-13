0ff574ebb9081b359bfc1e0f582cebcd03f366fb
Aditya Singh <adityasinghparmar60@gmail.com>
2026-01-13 18:24:34 +0530

    API Calling module added to handle GitHub API requests and data processing.

---
+++ api_calling.py
@@ -0,0 +1,26 @@
+import requests
+
+Token="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"
+repo="GitHub_Analytics_Tool"
+user="aditya-singh597"
+
+url=f"https://api.github.com/repos/{user}/{repo}/commits"
+
+headers={"Authorization": f"Bearer {Token}", "Accept": "application/vnd.github+json"}
+all_commits=[]
+page=1
+
+while True:
+    params={"per_page":100,"page":page}
+    response=requests.get(url,headers=headers,params=params)
+    data=response.json()
+    print(data)
+
+    if not data:
+        break
+
+    all_commits.extend(data)
+    page+=1
+
+
+print(f"Fetched {len(all_commits)} commits")
